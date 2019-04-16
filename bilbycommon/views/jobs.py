a69f6d2b0cf8810d4b073ccafe2f1e6ede1c826b
Shibli Saleheen <ssaleheen@swin.edu.au>
2019-04-16 15:52:44 +1000

    view job initial fix for the CW jobs

--- bilbycommon/views/jobs.py
+++ bilbycommon/views/jobs.py
@@ -357,9 +357,11 @@ def view_job(request, job_id):
             if 'copy' not in bilby_job.job_actions:
                 job = None
             else:
-                # create a bilby_job instance of the job
-                bilby_job = PEJob(job_id=job.id)
-                bilby_job.list_actions(request.user)
+                # create a full bilby_job instance of the job
+                if type(bilby_job) == CWJob:
+                    bilby_job = CWJob(job_id=job_id)
+                elif type(bilby_job) == PEJob:
+                    bilby_job = PEJob(job_id=job.id)
 
                 # Empty parameter dict to pass to template
                 job_data = {
@@ -446,10 +448,10 @@ def copy_job(request, job_id):
                 # create a full bilby_job instance of the job
                 # but not sure whether it is required for copying, because, we just need the job id for copying
                 # so, for the time being, it is commented out, otherwise, we can always reopen it.
-                if type(bilby_job) == CWJob:
-                    bilby_job = CWJob(job_id=job_id)
-                elif type(bilby_job) == PEJob:
-                    bilby_job = PEJob(job_id=job.id)
+                # if type(bilby_job) == CWJob:
+                #     bilby_job = CWJob(job_id=job_id)
+                # elif type(bilby_job) == PEJob:
+                #     bilby_job = PEJob(job_id=job.id)
 
                 job = bilby_job.clone_as_draft(request.user)
                 if not job:
